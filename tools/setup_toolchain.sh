dd6cd0b520a7dda33c6206e74290d6961127e972
Kartatz <105828205+Kartatz@users.noreply.github.com>
2023-04-29 23:29:21 -0300

    Update triplet in setup_toolchain.sh

--- tools/setup_toolchain.sh
+++ tools/setup_toolchain.sh
@@ -13,7 +13,7 @@ fi
 
 declare -r OBGGCC_CROSS_TAG="$(jq --raw-output '.tag_name' <<< "$(curl --retry 10 --retry-delay 3 --silent --url 'https://api.github.com/repos/AmanoTeam/obggcc/releases/latest')")"
 declare -r OBGGCC_CROSS_TARBALL='/tmp/obggcc.tar.xz'
-declare -r OBGGCC_CROSS_URL="https://github.com/AmanoTeam/obggcc/releases/download/${OBGGCC_CROSS_TAG}/x86_64-linux-gnu.tar.xz"
+declare -r OBGGCC_CROSS_URL="https://github.com/AmanoTeam/obggcc/releases/download/${OBGGCC_CROSS_TAG}/x86_64-unknown-linux-gnu.tar.xz"
 
 curl --retry 10 --retry-delay 3 --silent --location --url "${OBGGCC_CROSS_URL}" --output "${OBGGCC_CROSS_TARBALL}"
 tar --directory="$(dirname "${OBGGCC_CROSS_TARBALL}")" --extract --file="${OBGGCC_CROSS_TARBALL}"
