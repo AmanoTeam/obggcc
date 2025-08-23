ca8cd74cb73aa5c43ead45a7cb8bb50a361d0c14
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-08-23 03:40:37 -0300

    Bump version

--- build.sh
+++ build.sh
@@ -560,7 +560,7 @@ for target in "${targets[@]}"; do
 		--with-zstd="${toolchain_directory}" \
 		--with-bugurl='https://github.com/AmanoTeam/obggcc/issues' \
 		--with-gcc-major-version-only \
-		--with-pkgversion="OBGGCC v3.4-${revision}" \
+		--with-pkgversion="OBGGCC v3.5-${revision}" \
 		--with-sysroot="${toolchain_directory}/${triplet}" \
 		--with-native-system-header-dir='/include' \
 		--with-default-libstdcxx-abi='new' \
