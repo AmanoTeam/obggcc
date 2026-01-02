db744c9b020a61f716c24ccd0c846ebc42a001df
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-01-02 00:50:17 -0300

    Re-enable all cross-compilation targets

--- build.sh
+++ build.sh
@@ -165,15 +165,15 @@ declare -ra deprecated_targets=(
 	'sparc-unknown-linux-gnu'
 	'alpha-unknown-linux-gnu'
 	'hppa-unknown-linux-gnu'
+	'armv6-unknown-linux-gnueabi'
 )
 
 declare -ra targets=(
 	'x86_64-unknown-linux-gnu'
-	# 'aarch64-unknown-linux-gnu'
-	# 'arm-unknown-linux-gnueabi'
-	# 'armv6-unknown-linux-gnueabi'
-	# 'arm-unknown-linux-gnueabihf'
-	# 'i386-unknown-linux-gnu'
+	'aarch64-unknown-linux-gnu'
+	'arm-unknown-linux-gnueabi'
+	'arm-unknown-linux-gnueabihf'
+	'i386-unknown-linux-gnu'
 )
 
 declare -r PKG_CONFIG_PATH="${toolchain_directory}/lib/pkgconfig"
