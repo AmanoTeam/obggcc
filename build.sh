eccee3f25a1ba1dbf5f9ff0e510bfb83d477b035
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-03-14 01:33:46 -0300

    t 10

--- build.sh
+++ build.sh
@@ -585,9 +585,12 @@ if ! [ -f "${gcc_tarball}" ]; then
 		patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/gcc-10/0001-Fix-build-when-compiling-in-C-17-mode.patch"
 	fi
 	
+	if (( gcc_major == 11 )); then
+		patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/gcc-11/0001-Unpoison-calloc-on-musl-hosts.patch"
+	fi
+	
 	if (( gcc_major <= 11 )); then
 		patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/gcc-11/0001-Add-missing-sys-select.h-include-on-BSD.patch"
-		patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/gcc-11/0001-Unpoison-calloc-on-musl-hosts.patch"
 		patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/gcc-11/0001-Darwin-Arm64-Initial-support-for-the-self-host-driver.patch"
 	fi
 	
