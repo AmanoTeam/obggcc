5c86193b74764b61902ac54e577f09672f016026
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-03-26 18:27:43 -0300

    Support compiling GCC 4.1

--- arm-unknown-linux-gnueabihf.sh
+++ arm-unknown-linux-gnueabihf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare extra_configure_flags='--with-arch=armv7-a --with-fpu=vfpv3-d16 --with-float=hard --with-mode=arm' 
+declare extra_configure_flags='--with-arch=armv7-a --with-fpu=vfpv3-d16 --with-float=hard' 
 declare triplet='arm-unknown-linux-gnueabihf'
 
 declare sysroot='https://github.com/AmanoTeam/debian-sysroot/releases/latest/download/arm-unknown-linux-gnueabihf2.13.tar.xz'
