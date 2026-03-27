c1081e01174b6bf52e8b1c57a205a7181fa91427
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-03-26 22:51:43 -0300

    Support compiling GCC 4.1

--- build.sh
+++ build.sh
@@ -609,6 +609,10 @@ if ! [ -f "${gcc_tarball}" ]; then
 		patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/gcc-11/0001-Add-missing-sys-select.h-include-on-BSD.patch"
 	fi
 	
+	if (( gcc_major >= 4.1 )); then
+		patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/gcc-4.1/0001-Prevent-use-of-sys-wait.h-on-Windows.patch"
+	fi
+	
 	if (( gcc_major >= 7 && gcc_major <= 11 )); then
 		patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/gcc-7/0001-Darwin-Arm64-Initial-support-for-the-self-host-driver.patch"
 	fi
