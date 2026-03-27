1ba47ad3017106bd630460dee07f171be8614d88
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-03-26 23:21:46 -0300

    Support compiling GCC 4.1

--- build.sh
+++ build.sh
@@ -609,6 +609,10 @@ if ! [ -f "${gcc_tarball}" ]; then
 		patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/gcc-11/0001-Add-missing-sys-select.h-include-on-BSD.patch"
 	fi
 	
+	if (( gcc_major >= 4.1 )); then
+		patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/gcc-4.1/0001-Default-to-enable-twoprocess-for-MinGW32.patch"
+	fi
+	
 	if (( gcc_major >= 7 && gcc_major <= 11 )); then
 		patch --directory="${gcc_directory}" --strip='1' --input="${workdir}/patches/gcc-7/0001-Darwin-Arm64-Initial-support-for-the-self-host-driver.patch"
 	fi
@@ -1264,7 +1268,6 @@ for target in "${targets[@]}"; do
 		--enable-autolink-librt \
 		--enable-libgomp \
 		--enable-libstdcxx-verbose \
-		--enable-twoprocess \
 		--with-specs="${specs}" \
 		--with-pic \
 		--with-gnu-as \
