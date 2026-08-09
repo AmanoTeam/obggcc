3316f4610414df6cef817fb0909eecd2a13fbb2b
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-08-09 01:59:27 -0300

    a

--- build.sh
+++ build.sh
@@ -1419,8 +1419,8 @@ for target in "${targets[@]}"; do
 	fi
 	
 	if (( gcc_major == 2.95 )); then
-		printf "exec '%s' \"\${@}\"\n" '${triplet}2.3-gcc' > "${build_directory}/${triplet}-gcc"
-		printf "exec '%s' \"\${@}\"\n" '${triplet}2.3-g++' > "${build_directory}/${triplet}-g++"
+		printf "exec '%s' \"\${@}\"\n" "${triplet}2.3-gcc" > "${build_directory}/${triplet}-gcc"
+		printf "exec '%s' \"\${@}\"\n" "${triplet}2.3-g++" > "${build_directory}/${triplet}-g++"
 		
 		chmod +x "${build_directory}/${triplet}-"*
 	fi
