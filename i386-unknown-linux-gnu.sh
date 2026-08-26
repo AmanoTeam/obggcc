346230078dfdf9d33cdf2e5258cca2d9c631efaa
Kartatz <105828205+Kartatz@users.noreply.github.com>
2026-08-26 03:19:36 -0300

    Build x86 toolchains with --with-tune=intel

--- i386-unknown-linux-gnu.sh
+++ i386-unknown-linux-gnu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare extra_configure_flags='--with-arch=i586'
+declare extra_configure_flags='--with-arch=i586 --with-tune=intel'
 declare triplet='i386-unknown-linux-gnu'
 
 declare sysroot='https://github.com/AmanoTeam/debian-sysroot/releases/latest/download/i386-unknown-linux-gnu2.3.tar.xz'
--- x86_64-unknown-linux-gnu.sh
+++ x86_64-unknown-linux-gnu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-declare extra_configure_flags=''
+declare extra_configure_flags='--with-arch=x86-64 --with-tune=intel'
 declare triplet='x86_64-unknown-linux-gnu'
 
 declare sysroot='https://github.com/AmanoTeam/debian-sysroot/releases/latest/download/x86_64-unknown-linux-gnu2.3.tar.xz'
